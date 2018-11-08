028b79a5f0d3e44ab8ab9c8c43e7c5f94228681d
Naresh <naresh.narasimhalu@gmail.com>
2018-11-08 16:49:56 +0530

    test

--- Learning_Github/Learning_Github/Controllers/HomeController.cs
+++ Learning_Github/Learning_Github/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace Learning_Github.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult  Index()
         {
             return View();
         }
